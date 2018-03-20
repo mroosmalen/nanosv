ff45be85d7b58fb0ae7a63a7362bfe86896ce7b3
mroosmalen <m.vanroosmalen-2@umcutrecht.nl>
2018-03-20 15:29:00 +0100

    Don't skip first line in samtools depth output

--- nanosv/utils/coverage.py
+++ nanosv/utils/coverage.py
@@ -18,7 +18,7 @@ def calculate_coverage_bed():
     if 'sambamba' in NanoSV.opts_sambamba:
         cmd = NanoSV.opts_sambamba + " depth base --min-coverage=0 " + NanoSV.opts_bam + " -L " + NanoSV.opts_bed + " 2> /dev/null | awk '{if (NR!=1) print $3}'"
     elif 'samtools' in NanoSV.opts_sambamba:
-        cmd = NanoSV.opts_sambamba + " depth " + NanoSV.opts_bam + " -b " + NanoSV.opts_bed + " | awk '{if (NR!=1) print $3}'"
+        cmd = NanoSV.opts_sambamba + " depth " + NanoSV.opts_bam + " -b " + NanoSV.opts_bed + " | awk '{print $3}'"
     else:
         sys.exit('No sambamba or samtools found')
 
