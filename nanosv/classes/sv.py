012014817069550e4f0f3111d70e479193cc789d
Mark van Roosmalen <mroosmalen@cog146.op.umcutrecht.nl>
2018-11-19 12:54:30 +0100

    add read ids to info field

--- nanosv/classes/sv.py
+++ nanosv/classes/sv.py
@@ -29,6 +29,7 @@ class SV:
         self.qual = None
         self.filter = []
         self.info = {
+            'ALT_READ_IDS': [breakpoint.segment_1['qname']],
             'IMPRECISE': True,
             'END': [breakpoint.segment_2["pos"]],
             'SVTYPE': breakpoint.svtype,
@@ -56,7 +57,7 @@ class SV:
         self.breakpoints.append(breakpoint.id)
         self.pos.append(breakpoint.segment_1["pos"])
         self.info['END'].append(breakpoint.segment_2["pos"])
-        self.info['ALT_READ_IDS'].append(breakpoint.segment_1['rname'])
+        self.info['ALT_READ_IDS'].append(breakpoint.segment_1['qname'])
         self.format['DV'][0] += 1
         self.format['DV'][1] += 1
         self.format['VO'][0] += (1 - 10 ** (-breakpoint.segment_1["mapq"] / 10.0))
